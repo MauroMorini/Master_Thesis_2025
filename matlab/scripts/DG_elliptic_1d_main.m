d9d441a0af9bbf7932ac8e7b83e3e63dac1557f0
GeileToni <100771361+GeileToni@users.noreply.github.com>
2025-09-06 13:59:23 +0200

    working

--- matlab/scripts/DG_elliptic_1d_main.m
+++ matlab/scripts/DG_elliptic_1d_main.m
@@ -3,9 +3,11 @@ clc;clear;close all;
 
 % Imports
 import mesh.*
+import fem1d.*
 
 % initialize mesh
-Mesh = Mesh1d();
+Mesh = Mesh1d([0,1], [0.2, 0.002]);
+[nodes, boundary_nodes_idx, elements] = Mesh.getPet();
 
 % define function handles (real solution)   
 syms x
@@ -13,3 +15,9 @@ u_exact_handle = sin(x);
 f_exact_handle = diff(u_exact_handle, 2);
 u_exact_handle = matlabFunction(u_exact_handle);
 f_exact_handle = matlabFunction(f_exact_handle);
+c_handle = @(x) ones(size(x));
+
+% assemble matrices
+A = fem1d.stiffnessMatrix1D(nodes', elements, c_handle);
+load_v = fem1d.loadVectorLinear1D(nodes', elements, f_exact_handle);
+
